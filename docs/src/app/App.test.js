f6340ce018bfd640dfaee9e0c5ef990627dd67da
vasilenka <meepomodeon@gmail.com>
2019-01-30 22:45:43 +0700

    init dummy test 😜, I am learning, dont be so hard~

--- docs/src/app/App.test.js
+++ docs/src/app/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement
+} from 'react-testing-library';
+
 import App from './App';
 
-it('renders without crashing', () => {
+afterEach(cleanup);
+
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
