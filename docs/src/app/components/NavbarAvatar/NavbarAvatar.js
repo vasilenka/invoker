f0c936c892278964a9dd31d88fc845c7a9781328
Ongki Herlambang <meepomodeon@gmail.com>
2019-03-09 23:50:16 +0700

    add PopoutAlt and finish NavbarAvatar

--- docs/src/app/components/NavbarAvatar/NavbarAvatar.js
+++ docs/src/app/components/NavbarAvatar/NavbarAvatar.js
@@ -3,21 +3,34 @@ import React from 'react';
 import cx from 'classnames';
 
 import Image from '../../primitives/Image/Image';
+import Popout from './../../primitives/Popout/Popout';
+
+const content = (
+  <div className={styles.menuContainer}>
+    <li>hello world</li>
+    <li>hello world</li>
+    <li>hello world</li>
+  </div>
+);
 
 const NavbarAvatar = ({ children, src, className, ...restProps }) => {
   return (
-    <div
-      className={cx({
-        [styles.root]: true,
-        [className]: className
-      })}
-      {...restProps}
-    >
-      <div className={styles.container}>
-        <Image src={src} fit="cover" className={styles.picture} />
-        {children}
-      </div>
-    </div>
+    <Popout withArrow={false} bottomRight content={content}>
+      {(setVisible, visible) => (
+        <div
+          onClick={() => setVisible(!visible)}
+          className={cx({
+            [styles.root]: true,
+            [className]: className
+          })}
+          {...restProps}
+        >
+          <div className={styles.container}>
+            <Image src={src} fit="cover" className={styles.picture} />
+          </div>
+        </div>
+      )}
+    </Popout>
   );
 };
 
